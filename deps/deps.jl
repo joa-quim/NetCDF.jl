e41b02208e542d85c253db765c5bc7c9cc6786c7
Joaquim Luis <jmfluis@gmail.com>
2020-09-06 11:24:18 +0100

    Test that GMT is present and fix solution for *nixs

--- deps/deps.jl
+++ deps/deps.jl
@@ -1,10 +1,17 @@
-
+# Fish the binary dependency via GMT lib (except on Windows where we know it in advance)
+const gmtlib = Ref{String}()
+try
+	gmtlib[] = haskey(ENV,"GMT_LIBRARY") ?
+		ENV["GMT_LIBRARY"] : string(chop(read(`gmt --show-library`, String)))
+catch
+    error("This package can only be installed in systems that have GMT")
+end
 @static Sys.iswindows() ?
 	(Sys.WORD_SIZE == 64 ? (const libnetcdf = "netcdf4_w64") : (const libnetcdf = "netcdf4_w32")) : 
 	(
-		@static Sys.islinux() ? libnetcdf = split(readlines(pipeline(`ldd $s`, `grep libnetcdf`))[1])[3] :
+		Sys.isapple() ? libnetcdf = split(readlines(pipeline(`otool -L $gmtlib`, `grep libnetcdf`))[1])[1] :
 		(
-			Sys.isapple() ? libnetcdf = split(readlines(pipeline(`otool -L $s`, `grep libnetcdf`))[1])[1] :
-			libnetcdf = "libnetcdf"		# Default for other unixs
+		    Sys.isunix() ? libnetcdf = split(readlines(pipeline(`ldd $gmtlib`, `grep libnetcdf`))[1])[3] :
+			error("Don't know how to install this package in this OS.")
 		)
 	)
