60bc7523ec714bc52ae689cbc6dac8104b6bb25a
Joaquim Luis <jmfluis@gmail.com>
2020-09-07 01:04:06 +0100

    Fixes for finding the lib in *nix

--- deps/deps.jl
+++ deps/deps.jl
@@ -9,9 +9,9 @@ end
 @static Sys.iswindows() ?
 	(Sys.WORD_SIZE == 64 ? (const libnetcdf = "netcdf4_w64") : (const libnetcdf = "netcdf4_w32")) : 
 	(
-		Sys.isapple() ? libnetcdf = split(readlines(pipeline(`otool -L $gmtlib`, `grep libnetcdf`))[1])[1] :
+		Sys.isapple() ? (const libnetcdf = Symbol(split(readlines(pipeline(`otool -L $gmtlib`, `grep libnetcdf`))[1])[1])) :
 		(
-		    Sys.isunix() ? libnetcdf = split(readlines(pipeline(`ldd $gmtlib`, `grep libnetcdf`))[1])[3] :
+		    Sys.isunix() ? (const libnetcdf = Symbol(split(readlines(pipeline(`ldd $gmtlib`, `grep libnetcdf`))[1])[3])) :
 			error("Don't know how to install this package in this OS.")
 		)
 	)
